42ae1ff3eac81897e2045425980f608bca1b5bf4
Oscar <oscar@nodos.io>
2022-10-01 06:24:37 -0500

    [layout] - products

--- app/src/main/java/com/tecfit/gym_android/MainActivity.kt
+++ app/src/main/java/com/tecfit/gym_android/MainActivity.kt
@@ -2,10 +2,25 @@ package com.tecfit.gym_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.tecfit.gym_android.fragments.ProductActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val productsFragment = ProductActivity();
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        replaceFragment(productsFragment)
         setContentView(R.layout.activity_main)
     }
+
+    private fun replaceFragment(fragment:Fragment){
+        val fragmentManager = supportFragmentManager
+
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_products, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
