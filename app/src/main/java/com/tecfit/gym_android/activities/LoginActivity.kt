9663353a455d0904eb3b1a114a8ce8657cac6713
Oscar <oscar@nodos.io>
2022-10-04 15:38:31 -0500

    [fix] - layout products

--- app/src/main/java/com/tecfit/gym_android/activities/LoginActivity.kt
+++ app/src/main/java/com/tecfit/gym_android/activities/LoginActivity.kt
@@ -1,12 +1,23 @@
 package com.tecfit.gym_android.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.tecfit.gym_android.R
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var fl: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        fl = findViewById(R.id.login_forgot_password)
+
+        fl.setOnClickListener{
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+        }
     }
 }
